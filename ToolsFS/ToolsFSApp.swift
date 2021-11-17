b242bf63846cfc2e55d44fa5a101a4190bea9edd
Andrew Pouliot <andrew@darknoon.com>
2021-11-17 08:36:57 -0500

    Initial Commit

---
+++ ToolsFS/ToolsFSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolsFSApp.swift
+//  ToolsFS
+//
+//  Created by Andrew Pouliot on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct ToolsFSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
